a99c5aecef032994687fc14f1ce9bb7c8b67bced
Nikitas Stefanidis <61457804+EllinikosKafes@users.noreply.github.com>
2025-06-26 09:08:12 +0300

    Add files via upload
    Setting up

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "mylib.h"
+
+int main(void){
+
+    print_mycards();
+
+    return 0;
+}
---
+++ mylib.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "mylib.h"
+
+void print_mycards(){
+
+    printf("-------------\n"
+"|           |\n"
+"|           |\n"
+"|           |\n"
+"|           |\n"
+"|           |\n"
+"|           |\n"
+"|           |\n"
+"-------------\n");
+}
